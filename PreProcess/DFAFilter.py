d9736ed15ae6883aac89e4c70018fda437a0b1ac
sx <827312773@qq.com>
2020-01-02 11:24:32 +0800

    name

--- PreProcess/DFAFilter.py
+++ PreProcess/DFAFilter.py
@@ -2,13 +2,13 @@
 
 import time
 
-from pybloomfilter import BloomFilter
+# from pybloomfilter import BloomFilter
 
 time1 = time.time()
 
 
 # DFA算法
-class DFAFilter():
+class DFAFilter:
     def __init__(self):
         self.keyword_chains = {}
         self.delimit = '\x00'
@@ -38,6 +38,7 @@ class DFAFilter():
         with open(path, encoding='utf-8') as f:
             for keyword in f:
                 self.add(str(keyword).strip())
+        f.close()
 
     def filter(self, message, repl="*"):
         message = message.lower()
@@ -64,20 +65,34 @@ class DFAFilter():
 
         return ''.join(ret)
 
-    def bloomFilter(self, text):
-        bffilter = BloomFilter(1000000, 0.001, 'bf_test.bloom')
-        level = self.keyword_chains
-        if text in level:
-            return True
-        return False
+    def isNormalFilter(self, message):
+        message = message.lower()
+        start = 0
+        while start < len(message):
+            level = self.keyword_chains
+            step_ins = 0
+            for char in message[start:]:
+                if char in level:
+                    step_ins += 1
+                    if self.delimit not in level[char]:
+                        level = level[char]
+                    else:
+                        start += step_ins - 1
+                        return False
+                else:
+                    break
+            else:
+                return True
+            start += 1
+        return True
 
 
 if __name__ == "__main__":
     gfw = DFAFilter()
-    path = "F:/文本反垃圾算法/sensitive_words.txt"
+    path = "../UnNormal.txt"
     gfw.parse(path)
-    text = "新疆骚乱苹果新品发布会雞八"
-    result = gfw.filter(text)
+    text = '曰本真人性做爰在线观看_免费人做人爱的视频_免费观看-女人天堂'
+    result = gfw.isNormalFilter(text)
 
     print(text)
     print(result)
--- PreProcess/dataLabel.py
+++ PreProcess/dataLabel.py
@@ -4,10 +4,16 @@ from lxml import html
 import pymysql
 import traceback
 
+from PreProcess.DFAFilter import DFAFilter
+
 db_info = {'host': '10.0.3.11', 'port': 3306, 'user': 'root', 'passwd': 'Ntci@85418825', 'dbname': 'sx'}
 db = pymysql.connect(host=db_info['host'], port=db_info['port'], user=db_info['user'], passwd=db_info['passwd'],
                      db=db_info['dbname'])
 c = db.cursor()
+
+gfw = DFAFilter()
+path = "../UnNormal.txt"
+gfw.parse(path)
 
 
 def readUnlabeledData():
@@ -15,6 +21,7 @@ def readUnlabeledData():
     c.execute(sql)
     results = c.fetchall()
     data = [i[0] for i in results]
+    c.close()
     return data
 
 
@@ -23,17 +30,18 @@ def readLabeledData():
     c.execute(sql)
     results = c.fetchall()
     data = [i[0] for i in results]
+    c.close()
     return data
 
 
 def label404():
     sql = """update domain_headers set flag = 3 where keywords="" """
     c.execute(sql)
+    c.close()
 
 
 def isYGP(text):
-    
-    pass
+    return gfw.isNormalFilter(text)
 
 
 def labelUnNormal(data):
@@ -41,9 +49,16 @@ def labelUnNormal(data):
         domain = data[i]["domain"]
         description = data[i]["description"]
         keywords = data[i]["keywords"]
-        isDescriptionNormal = isYGP(description)
-        isKeywordsNormal = isYGP(keywords)
-        if isDescriptionNormal == False or isKeywordsNormal == False:
+        is_description_normal = isYGP(description)
+        is_keywords_normal = isYGP(keywords)
+        if is_description_normal == False or is_keywords_normal == False:
             sql = """update domain_headers set flag = 2 where  domain = "{}" and description = "{}" """ \
                 .format(domain, description)
             c.execute(sql)
+            c.close()
+
+
+data = readLabeledData()
+print(len(data))
+for i in range(len(data)):
+    print("关键词"+data[i]['keywords']+"描述"+data[i]['description'])
